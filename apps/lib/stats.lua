0ae5c24d24c634f6cd1e1d03782019d6a679750a
songweihang <898095822@qq.com>
2016-04-20 16:52:57 +0800

    格式化样式问题

--- apps/lib/stats.lua
+++ apps/lib/stats.lua
@@ -1,16 +1,14 @@
 -- Copyright (C) 2016-2016 WeiHang Song (Jakin)
 -- 此库需要在log_by_lua阶段中执行，主要实现获取HTTP请求数据统计
 
-local setmetatable 				= setmetatable
---local assert 					= assert
-local ngxshared 				= ngx.shared
-local ngxfind                   = ngx.re.find
-local sub                       = string.sub
---local strfind                   = string.find
-local strformat 				= string.format
-local tonumber                  = tonumber
-local error                     = error
-local SHARED_NAMES 				= {}
+local setmetatable    = setmetatable
+local ngxshared       = ngx.shared
+local ngxfind         = ngx.re.find
+local sub             = string.sub
+local strformat       = string.format
+local tonumber        = tonumber
+local error           = error
+local SHARED_NAMES    = {}
 
 SHARED_NAMES['all'] = {
 	["keys"] = "stats_all_keys",
@@ -39,7 +37,6 @@ local _M = { _VERSION = '0.02' }
 local mt = { __index = _M }
 
 local function _add_num_cache(key,rule)
-
 	local shareddict = SHARED_NAMES[rule]
 
 	if shareddict == nil then
@@ -59,7 +56,6 @@ end
 
 
 local function _flush_key(key,rule)
-
 	local shareddict = SHARED_NAMES[rule]
 
 	if shareddict == nil then
@@ -77,16 +73,15 @@ end
 
 
 function _M.new(self,uri,status,request_time,upstream_response_time)
-    
-    --assert(status >0 and uri >= 0 and request_time >0 and upstream_response_time > 0 and host >0 )
-    local self = {
-    	uri    = uri or "-",
-    	status = tonumber(status) or 499,
-        request_time = request_time or 0,
-        upstream_response_time = tonumber(upstream_response_time) or 0,
-    }
+	--assert(status >0 and uri >= 0 and request_time >0 and upstream_response_time > 0 and host >0 )
+	local self = {
+		uri    = uri or "-",
+		status = tonumber(status) or 499,
+	    request_time = request_time or 0,
+	    upstream_response_time = tonumber(upstream_response_time) or 0,
+	}
 
-    return setmetatable(self, mt)
+	return setmetatable(self, mt)
 end
 
 
@@ -105,7 +100,7 @@ local function incr(self,key,rule)
 	if status == 499 then
 		return
 	end
-	
+
 	-- 初始化计数器
 	_add_num_cache(key,rule)
 
@@ -158,10 +153,10 @@ end
 function _M.incr_match(self,body,match)
 	-- 获取正则特殊定制统计
 	local from,to,err = ngxfind(body, match,"jo")
-    if from then 
-        local api = sub(body,from,to)
-        incr(self,api,"match")
-    end
+	if from then 
+	    local api = sub(body,from,to)
+	    incr(self,api,"match")
+	end
 end
 
 
@@ -228,8 +223,8 @@ function _M.read_key_lists(self,rule)
 	for i, key in ipairs(keys) do
 		local ratio = read_key_result(self,key,rule)
 		if ratio ~= nil then
-  			tableinsert(lists,ratio)
-  		end
+				tableinsert(lists,ratio)
+			end
 	end
 
 	return lists
