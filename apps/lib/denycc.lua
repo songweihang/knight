f3c7d5ede560707422f3bf9ed9ec9257a8f7f0c5
songweihang <898095822@qq.com>
2017-09-26 11:24:54 +0800

    修复极端情况可能出现的数据不一致问题

--- apps/lib/denycc.lua
+++ apps/lib/denycc.lua
@@ -12,6 +12,9 @@ local denycc_rate_conf       = system_conf.denycc_rate_conf
 local ngxshared              = ngx.shared
 local denycc_conf            = ngxshared.denycc_conf
 local cache                  = require "apps.resty.cache"
+local ngx_log                = ngx.log
+local string_format          = string.format
+local tonumber               = tonumber
 
 _M.denycc_run = function()
     local denycc_rate_ts         = denycc_conf:get('denycc_rate_ts') or denycc_rate_conf.ts
@@ -19,27 +22,50 @@ _M.denycc_run = function()
     local ip_parser              = require "apps.lib.ip_parser"
     local ip                     = ip_parser:get()
     local ts                     = math.ceil(ngx.time()/denycc_rate_ts)
-    local limit                  = 'LIMIT:' .. ip .. ':' .. ts
+    local limit                  = string_format("LIMIT:%s:%s", ip, ts)
 
+    _M.rate_limit(limit,denycc_rate_ts,denycc_rate_request)
+end
+
+_M.rate_limit = function(limit,denycc_rate_ts,denycc_rate_request)
     local red = cache:new(redis_conf)
     local ok, err = red:connectdb()
     if not ok then
         return 
     end
 
-    local hit, err = red.redis:incr(limit)
-
-    if hit == 1 then
-        red.redis:expire(limit,denycc_rate_ts)
+    local exists, err = red.redis:exists(limit)
+    if err then
+      ngx_log(ngx.ERR, "failed to query Redis: ", err)
+      return 
     end
     
+    red.redis:init_pipeline((not exists or exists == 0) and 2 or 1)
+    red.redis:incr(limit)
+    if not exists or exists == 0 then
+      red.redis:expire(limit,denycc_rate_ts)
+    end
+
+    local _, err = red.redis:commit_pipeline()
+    if err then
+      ngx_log(ngx.ERR, "failed to commit pipeline in Redis: ", err)
+      return 
+    end
+      
+    local hit, err = red.redis:get(limit)
+    if not hit then
+      ngx_log(ngx.ERR, "failed to query Redis: ", err)
+      return 
+    end
+
     red:keepalivedb()
 
-    if hit >= denycc_rate_request then
+    if tonumber(hit) >= denycc_rate_request then
+        -- return ngx.exit(ngx.HTTP_FORBIDDEN);
         ngx.header["Content-Type"] = "text/html; charset=UTF-8"
         ngx.header["NGX-DENYCC-HIT"] = hit
         ngx.exit(429)
-        ngx.log(ngx.ERR, "denycc ip: ", ip)
+        ngx_log(ngx.ERR, "denycc ip: ", ip)
         return
     else
         --ngx.header["Content-Type"] = "text/html; charset=UTF-8"
@@ -47,6 +73,5 @@ _M.denycc_run = function()
         --ngx.say(limit .. ":" .. hit)    
     end
 end
-
 
 return _M
